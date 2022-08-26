71b80667be313e1081ca496fef58df4c90d18430
Chan Lee <clee09818@gmail.com>
2022-08-25 22:35:07 -0400

    commit tree 11194e559214470f3ff6a62f59833cfe642ea3d0

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+
+
+
+
+function submitData(name, email){
+
+    const myObj = {
+        name: name,
+        email: email,
+    }
+
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(myObj),
+    };
+
+
+  return fetch('http://localhost:3000/users', configObject)
+        .then(response => response.json())
+        .then(function(data){
+                document.body.textContent = data.id;
+        })
+
+
+
+        .catch(function(error){
+            document.body.textContent = error.message;
+        })
+};
